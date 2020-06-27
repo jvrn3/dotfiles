acea199323657bdd0d6747e1616f6139efc45a8b
Joao Vitor Roma Neto <jvrn@protonmail.com>
2020-06-26 22:36:00 -0300

    just need to make it work

--- install.sh
+++ install.sh
@@ -2,35 +2,75 @@
 
 ########################
 
-####### You can find the code here: 
+####### You can find the code here:
 #### http://blog.smalleycreative.com/tutorials/using-git-and-github-to-manage-your-dotfiles/
 
+func_install() {
+	if pacman -Qi $1 &> /dev/null; then
+		tput setaf 2
+  		echo "###############################################################################"
+  		echo "################## The package "$1" is already installed"
+      	echo "###############################################################################"
+      	echo
+		tput sgr0
+	else
+    	tput setaf 3
+    	echo "###############################################################################"
+    	echo "##################  Installing package "  $1
+    	echo "###############################################################################"
+    	echo
+    	tput sgr0
+    	sudo pacman -S --noconfirm --needed $1
+    fi
+}
+
+distribution=`cat /etc/*release | head -n 1 | awk '{print $1}'`
+list=(
+tmux
+)
+
+for name in "${list[@]}" ; do
+	tput setaf 3;echo "Installing package " $name;tput sgr0;
+	func_install $name
+done
+
+echo ${distribution}
+green=`tput setaf 2`
+reset=`tput sgr0`
 dir=~/dotfiles
 olddir=~/dotfiles_old
-files="gvimrc tmux.conf vimrc config/nvim tmux/theme "
+files="gvimrc tmux.conf config/nvim tmux/theme "
 
 if [ ! -d ~/.config ]; then
 	echo "Creating config directory"
 	mkdir ~/.config
 fi
-if [ ! -d ~/.tmux]; then
+if [ ! -d ~/.tmux ]; then
     echo "Creating ~/.tmux/"
     mkdir ~/.tmux
+fi
 
-echo "Creatig $olddir for backup of existing files" 
+echo "Creatig $olddir for backup of existing files"
 mkdir -p $olddir
 echo "#################"
 echo "Done"
 
-echo "You should have nodejs and pip3 installed"
+echo "${green}You should have nodejs and pip3 installed${reset}"
 cd $dir
 
-for file in $files; do
-    echo "checking if there is a file named $file"
-    if [ -f $file ]; then
+for file in $files;
+do
+    echo "checking if there is a file or directory named $file"
+    if [ -e ~/.$file ]; then
         echo "Movig $file to $olddir/"
         mv ~/.$file $olddir
+        echo "Creating symlink to $file in home directory"
+        ln -s $dir/$file ~/.$file
+    else
+        echo "Creating symlink to $file in home directory"
+        ln -s $dir/$file ~/.$file
     fi
-	echo "Creating symlink to $file in home directory"
-	ln -s $dir/$file ~/.$file
+
+
+
 done
