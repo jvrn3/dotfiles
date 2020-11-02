7d9270f1242ec6c2971f4acfe64de0b225a7b012
Joao Vitor <jvrn@protonmail.com>
2020-11-02 16:22:40 -0300

    improve software_install.sh

--- software_install.sh
+++ software_install.sh
@@ -1,5 +1,25 @@
-sudo pacman -S neovim --noconfirm
+#!/bin/bash
 
-yay -S dust --noconfirm
-yay -S bat --noconfirm
-yay -S fd --noconfirm
+func_install(){
+    echo "Installing $1"
+    sudo pacman -S --noconfirm --needed $1
+}
+
+list_pacman=(
+    exa
+    mdcat
+)
+list_yay=(
+    dust
+    bat
+    fd
+    neovim-nightly
+)
+
+for name in "${list_pacman[@]}" ; do
+    func_install $name
+done
+
+for name in "${list_yay[@]}" ; do
+    func_install $name
+done
