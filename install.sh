69e16adc0bc5518b261d5d7a895eb431d6325513
Joao Vitor Neto <joao.neto@softplan.com.br>
2024-07-10 14:42:53 -0300

    add alacritty.toml

--- install.sh
+++ install.sh
@@ -11,16 +11,16 @@ reset=$(tput sgr0)
 
 dir=~/dotfiles
 olddir=~/dotfiles_old
-files="tmux.conf  tmux/theme alacritty.yml ideavimrc gitconfig"
+files="tmux.conf  tmux/theme alacritty.toml ideavimrc gitconfig"
 
 if [ ! -d ~/.config ]; then
-	echo "Creating config directory"
-	mkdir ~/.config
+  echo "Creating config directory"
+  mkdir ~/.config
 fi
 
 if [ ! -d ~/.tmux ]; then
-	echo "Creating ~/.tmux/"
-	mkdir ~/.tmux
+  echo "Creating ~/.tmux/"
+  mkdir ~/.tmux
 fi
 
 echo "Creatig $olddir for backup of existing files"
@@ -32,16 +32,16 @@ echo "${green}You need nodejs and pip3 installed${reset}"
 cd $dir
 
 for file in $files; do
-	echo "checking if there is a file or directory named $file"
-	if [ -e ~/.$file ]; then
-		echo "Movig $file to $olddir/"
-		mv ~/.$file $olddir
-		echo "Creating symlink to $file in home directory"
-		ln -s $dir/$file ~/.$file
-	else
-		echo "Creating symlink to $file in home directory"
-		ln -s $dir/$file ~/.$file
-	fi
+  echo "checking if there is a file or directory named $file"
+  if [ -e ~/.$file ]; then
+    echo "Movig $file to $olddir/"
+    mv ~/.$file $olddir
+    echo "Creating symlink to $file in home directory"
+    ln -s $dir/$file ~/.$file
+  else
+    echo "Creating symlink to $file in home directory"
+    ln -s $dir/$file ~/.$file
+  fi
 
 done
 #echo "moving xfce4 keyboard settings"
@@ -53,4 +53,3 @@ done
 #ln -s "$dir/xfce4-keyboard-shortcuts.xml" "$xfce4_config_folder/xfce4-keyboard-shortcuts.xml"
 
 #git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-
